363a289fffc06e9c37c9843eee3d2fb2b90499df
Santiago Cardona <santiagocardo80@gmail.com>
2021-07-03 14:42:12 -0500

    Home page tests

--- test/github_pr_mentions_web/live/page_live_test.exs
+++ test/github_pr_mentions_web/live/page_live_test.exs
@@ -1,11 +1,36 @@
 defmodule GithubPrMentionsWeb.PageLiveTest do
-  use GithubPrMentionsWeb.ConnCase
+  use GithubPrMentionsWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+  describe "with not logged-in user" do
+    test "disconnected and connected render", %{conn: conn} do
+      {:ok, page_live, disconnected_html} = live(conn, "/")
+
+      assert disconnected_html =~ "Sign in with GitHub"
+      assert render(page_live) =~ "Sign in with GitHub"
+    end
+  end
+
+  describe "with a logged-in user" do
+    setup %{conn: conn} do
+      user = %{login: "santiagocardo80", access_token: "test token"}
+
+      conn =
+        assign(conn, :current_user, user)
+        |> Plug.Test.init_test_session(%{})
+        |> put_session(:current_user, user)
+
+      {:ok, conn: conn, user: user}
+    end
+
+    test "disconnected and connected render", %{conn: conn} do
+      {:ok, page_live, disconnected_html} = live(conn, "/")
+
+      search_btn = "<button type=\"submit\" phx-disable-with=\"Searching...\">Search</button>"
+
+      assert disconnected_html =~ search_btn
+      assert render(page_live) =~ search_btn
+    end
   end
 end
