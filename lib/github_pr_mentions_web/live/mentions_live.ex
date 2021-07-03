c3c3e47b7383f55a47cac180a2ae4f0ed109f8c0
Santiago Cardona <santiagocardo80@gmail.com>
2021-07-03 14:37:17 -0500

    Integrate Mentions GenServer with Live View

--- lib/github_pr_mentions_web/live/mentions_live.ex
+++ lib/github_pr_mentions_web/live/mentions_live.ex
@@ -1,11 +1,42 @@
 defmodule GithubPrMentionsWeb.MentionsLive do
   use GithubPrMentionsWeb, :live_view
 
+  alias GithubPrMentions.Mentions
+
   @impl true
-  def mount(%{"username" => username}, _session, socket) do
+  def mount(%{"repo" => repo, "username" => username}, %{"current_user" => current_user}, socket) do
+    Mentions.get_mentions(repo, username, current_user.access_token)
+
+    if connected?(socket), do: Mentions.subscribe("lobby")
+
+    Process.send_after(self(), :check_prs, 90_000)
+
     {:ok,
      socket
      |> assign(prs: [], username: username)
      |> put_flash(:info, "Searching mentions..."), temporary_assigns: [prs: []]}
+  end
+
+  @impl true
+  def handle_info({Mentions, :new_mentions, {pr_number, mentions, username}}, socket) do
+    if username == socket.assigns.username do
+      send_update(GithubPrMentionsWeb.ShowMentions, id: pr_number, mentions: mentions)
+
+      {:noreply,
+       socket
+       |> assign(:prs, [pr_number])
+       |> put_flash(:info, "Looking for more mentions...")}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_info(:check_prs, socket) do
+    if socket.assigns.prs == [] do
+      {:noreply, put_flash(socket, :error, "No mentions found!")}
+    else
+      {:noreply, socket}
+    end
   end
 end
--- lib/github_pr_mentions_web/live/page_live.ex
+++ lib/github_pr_mentions_web/live/page_live.ex
@@ -5,6 +5,7 @@ defmodule GithubPrMentionsWeb.PageLive do
   def mount(_params, session, socket) do
     oauth_github_url = ElixirAuthGithub.login_url(%{scopes: ["user:email"]})
     current_user = session["current_user"] || nil
+    GithubPrMentions.Mentions.clean()
 
     {:ok, assign(socket, oauth_github_url: oauth_github_url, current_user: current_user)}
   end
